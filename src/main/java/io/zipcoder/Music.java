1b19a45e9620bb4b9c2ab2ee5254110ccbee4bc6
oahmed105 <oahmed10598@gmail.com>
2023-07-26 14:01:25 -0400

    passing one test so far

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,22 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int targetIndex = 0;
+        for (int i = 0; i < playList.length; i++) {
+            if (playList[i].equals(selection)) {
+                targetIndex = i;
+                break;
+            }
+        }
+
+        int clockwiseSteps = (targetIndex - startIndex + playList.length) % playList.length;
+        int counterclockwiseSteps = (startIndex - targetIndex + playList.length) % playList.length;
+
+        return Math.min(clockwiseSteps, counterclockwiseSteps);
     }
 }
